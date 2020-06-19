6e45256f5d826f393f3954b83f89f3cc0d099507
Daniel Sosa <daniel.soalvirde@gmail.com>
2020-06-19 11:04:36 -0500

    Revisar la cobertura de Test en tu producto

---
+++ movies-api/test/utils.buildMessage.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const buildMessage = require('../utils/buildMessage')
+
+describe('utils - buildMessage', () => {
+ describe('when receives an entity and action', () => {
+     it('should return the respective message', () => {
+         const result = buildMessage('movie', 'create')
+         const expect = 'movie created'
+         assert.strictEqual(result, expect)
+     });
+ })
+ 
+ describe('when receives an entity and action and is a list', () => {
+     it('should return the respective message with the entity in plural', () => {
+         const result = buildMessage('movie', 'list')
+         const expected = 'movies listed'
+         assert.strictEqual(result, expected)
+     });
+     
+ })
+ 
+    
+})
